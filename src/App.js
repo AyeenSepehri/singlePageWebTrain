e33afd5892e2b70547f9f962a70a35aa3a97e1e5
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-05-27 16:10:55 +0430

    first commit of my single page react training project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SecondPage from "./pages/SecondPage";
+import LastPage from "./pages/LastPage";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Route path="/">
+          <HomePage/>
+        </Route>
+        <Route path="/second">
+          <SecondPage/>
+        </Route>
+        <Route path="/last">
+          <LastPage/>
+        </Route>
+      
     </div>
   );
 }
---
+++ src/pages/LastPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LastPage() {
+  return (
+    <div>LastPage</div>
+  )
+}
+
+export default LastPage
