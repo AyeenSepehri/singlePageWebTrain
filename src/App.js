2f55934c96cdde36c02c540bc1002aafefe0f7c4
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-05-28 21:39:27 +0430

    useing react router v6 in project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route , Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SecondPage from "./pages/SecondPage";
 import LastPage from "./pages/LastPage";
+import NavBar from "./components/layout/NavBar";
 function App() {
   return (
     <div className="App">
-        <Route path="/">
-          <HomePage/>
-        </Route>
-        <Route path="/second">
-          <SecondPage/>
-        </Route>
-        <Route path="/last">
-          <LastPage/>
-        </Route>
-      
+      <NavBar/>
+      <Routes>
+        <Route path="/" element = {<HomePage/>} />          
+        <Route path="/second" element={<SecondPage/>}/>          
+        <Route path="/last" element= {<LastPage/>}/>
+      </Routes>
     </div>
   );
 }
