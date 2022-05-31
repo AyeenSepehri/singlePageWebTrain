9209904a26f5483378dc643bebd8c8d128aacb92
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-05-31 14:21:59 +0430

    create a new component and import it to second page and define form for second page and a part of logic of it for catch data from form and pass it to a new object

---
+++ src/components/Instruments/NewInstrumentFrom.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useRef } from 'react'
+import Card from "../ui/Card"
+import classes from "./NewInstrumentForm.module.css"
+function NewInstrumentFrom() {
+    const nameInputRef = useRef()
+    const imageInputRef = useRef()
+    const typeInputRef = useRef()
+    const infoInputRef = useRef()
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const enteredName = nameInputRef.current.value
+        const enteredImage = imageInputRef.current.value
+        const enteredType = typeInputRef.current.value
+        const enteredInfo = infoInputRef.current.value
+        const InstrumentData = {
+            name : enteredName,
+            image : enteredImage,
+            type : enteredType,
+            info : enteredInfo
+        }
+        console.log(InstrumentData)
+    }
+  return (
+    <Card>
+        <form className={classes.form} onSubmit={submitHandler}>
+            <div className={classes.control}>
+                <label htmlFor='name'>Instrument's name</label>
+                <input type="text" id="name" required ref={nameInputRef}/>
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='image'>Instrument's image</label>
+                <input type="url" id="image" required ref={imageInputRef}/>
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='type'>Instrument's type</label>
+                <input type="text" id="type" required ref={typeInputRef}/>
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='info'>Instrument's info</label>
+                <textarea id="info" rows="5" required ref={infoInputRef}></textarea>
+            </div>
+            <div className={classes.action}>
+                <button>add instrument</button>
+            </div>
+        </form>
+    </Card>
+  )
+}
+
+export default NewInstrumentFrom
--- src/pages/SecondPage.js
+++ src/pages/SecondPage.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import NewInstrumentFrom from '../components/Instruments/NewInstrumentFrom'
 function SecondPage() {
   return (
-    <div>SecondPage</div>
+    <div>
+      <h1>new instrument</h1>
+      <NewInstrumentFrom/>
+    </div>
   )
 }
 
