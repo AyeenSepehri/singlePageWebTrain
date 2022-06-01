6c768bd1f0685218aade368a5aa31f7661fd67ff
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-06-01 20:22:43 +0430

    posting data to firebase database and set navigate to home page

--- src/pages/SecondPage.js
+++ src/pages/SecondPage.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import NewInstrumentFrom from '../components/Instruments/NewInstrumentFrom'
+import { useNavigate } from 'react-router-dom'
+import NewInstrumentForm from '../components/Instruments/NewInstrumentForm'
+
+
 function SecondPage() {
+  const navigate = useNavigate();
+  const AddInstrument = (InstrumentData) => {
+
+      fetch(
+        "https://addinstrument-train-default-rtdb.firebaseio.com/Instrument.json",
+        {
+          method : "POST",
+          body : JSON.stringify(InstrumentData),
+          headers : {
+            "Content-Type" : "application/json"
+          },
+        }
+      ).then(() => {
+        navigate("/")
+      })
+  }
   return (
     <div>
       <h1>new instrument</h1>
-      <NewInstrumentFrom/>
+      <NewInstrumentForm postData = {AddInstrument}/>
     </div>
   )
 }
