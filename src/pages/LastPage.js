c312bde86f55063c6d499ba24504b8eef4af7ac5
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-06-07 19:54:56 +0430

    adding content and import context to favoritePage

--- src/pages/LastPage.js
+++ src/pages/LastPage.js
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React, {useContext} from 'react'
+import InstrumentationCard from '../components/Instruments/InstrumentationCard'
+import FavoriteContext from "../store/favorites.context"
 function LastPage() {
+  const FavoriteCtx = useContext(FavoriteContext)
+  let content ;
+  if(FavoriteCtx.totalFavorites === 0){
+    content = <p>you don't have favorite instrument yet,find your favorite instrument as soon as possible!</p> 
+  }else{
+    content =  <InstrumentationCard Items ={FavoriteCtx.favorites} />
+  }
   return (
-    <div>LastPage</div>
-  )
+    <div>
+    <h1>FavoritePage</h1>
+    {content}
+    </div>
+    
+    )
 }
 
 export default LastPage
