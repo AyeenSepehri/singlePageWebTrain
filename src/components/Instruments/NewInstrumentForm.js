4ae08696ed6baea9e704cec60f7a200a5bc72512
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-06-08 19:15:02 +0430

    adding useForm and define a part of validate form

--- src/components/Instruments/NewInstrumentForm.js
+++ src/components/Instruments/NewInstrumentForm.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import { useRef } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import Card from "../ui/Card"
 import classes from "./NewInstrumentForm.module.css"
+const InstrumentSchema = yup.object().shape({
+    name : yup.string().required("fill name correct"),
+    image : yup.string().url().required("fill url correct"),
+    type : yup.string().required("fill type correct"),
+    info : yup.string().required("fill info correct"),
+}) 
 function NewInstrumentForm(props) {
+    const {register,handleSubmit,formState:{errors}} = useForm({resolver : yupResolver(InstrumentSchema)})
+    // console.log(errors)
     const nameInputRef = useRef()
     const imageInputRef = useRef()
     const typeInputRef = useRef()
     const infoInputRef = useRef()
-
     const submitHandler = (event) => {
         event.preventDefault();
         const enteredName = nameInputRef.current.value
@@ -27,19 +37,23 @@ function NewInstrumentForm(props) {
         <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.control}>
                 <label htmlFor='name'>Instrument's name</label>
-                <input type="text" id="name" required ref={nameInputRef}/>
+                <input type="text" id="name" {...register("name")} ref={nameInputRef}/>
+                {errors.name && <p>{errors.name.message}</p>}
             </div>
             <div className={classes.control}>
                 <label htmlFor='image'>Instrument's image</label>
-                <input type="url" id="image" required ref={imageInputRef}/>
+                <input type="url" id="image" {...register("image")} ref={imageInputRef}/>
+                {errors.image && <p>{errors.image.message}</p>}
             </div>
             <div className={classes.control}>
                 <label htmlFor='type'>Instrument's type</label>
-                <input type="text" id="type" required ref={typeInputRef}/>
+                <input type="text" id="type" {...register("type")} ref={typeInputRef}/>
+                {errors.type && <p>{errors.type.message}</p>}
             </div>
             <div className={classes.control}>
                 <label htmlFor='info'>Instrument's info</label>
-                <textarea id="info" rows="5" required ref={infoInputRef}></textarea>
+                <textarea id="info" rows="5" {...register("info")} ref={infoInputRef}></textarea>
+                {errors.info && <p>{errors.info.message}</p>}
             </div>
             <div className={classes.action}>
                 <button>add instrument</button>
